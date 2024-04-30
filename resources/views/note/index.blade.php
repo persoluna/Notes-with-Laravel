815286ae12166cba7343928261fc77cbf9655937
persoluna <shakibmunshi007@outlook.com>
2024-04-30 17:08:15 +0530

    Finished

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-app-layout>
     <div class="note-container py-12">
         <a href="{{ route('note.create') }}" class="new-note-btn">
             New Note
@@ -24,4 +24,4 @@
 
         {{ $notes->links() }}
     </div>
-</x-layout>
+</x-app-layout>
